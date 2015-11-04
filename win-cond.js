ffd403a2efdfb63cf947f1959c884a811ae462b1
asngai <andrewngai.ucsb@gmail.com>
2015-11-04 12:35:19 -0800

    win_cond_js

--- win-cond.js
+++ win-cond.js
@@ -44,9 +44,9 @@ var win_condition = function(){
     self.check_row_win = function(board, turn){
         var row_length = Math.sqrt(board.length);
         var num_rows = Math.sqrt(board.length);
-        var index = (i * row_length) + j;
         for(var i=0;i<row_length - 1;i++){
             var rows_filled = 0;
+            var index = (i * row_length) + j;
             for(var j=0;j<num_rows - 1; j++){
                 if(board[index][turn]){
                     rows_filled++;
@@ -65,16 +65,16 @@ var win_condition = function(){
         var num_cols = Math.sqrt(board.length);
         var index = (i * col_length) + j;
         for(var i=0;i<col_length - 1;i++){
-            var rows_filled = 0;
-            for(var j=0;j<num_rows - 1; j++){
+            var cols_filled = 0;
+            for(var j=0;j<num_cols - 1; j++){
                 if(board[index][turn]){
-                    rows_filled++;
+                    cols_filled++;
                 }
                 else{
                     break;
                 }
             }
-            if(rows_filled == row_length){
+            if(cols_filled == col_length){
                 return true;
             }
         }
