0589e898b9fd2cd9294d9e56658fd2aec2bcc6e9
asngai <andrewngai.ucsb@gmail.com>
2015-11-04 11:26:55 -0800

    win_condition_start

---
+++ win-cond.js
@@ -0,0 +1,50 @@
+var board_sq_3 = [
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}
+];
+
+var board_sq_4 = [
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}
+];
+
+var board_sq_5 = [
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}
+];
+
+var board_sq_6 = [
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},{'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+    {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
+];
+var win_condition = function(){
+    self = this;
+    self.check_board = function(board, turn){
+        var row_length,
+            num_rows = Math.sqrt(board.length);
+
+
+    }
+}
