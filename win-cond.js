45227bfc8137efd4fe61a4efa9c3379c9ad614a0
asngai <andrewngai.ucsb@gmail.com>
2015-11-04 13:48:36 -0800

    win_cond_js - debug req

--- win-cond.js
+++ win-cond.js
@@ -39,6 +39,7 @@ var board_sq_6 = [
     {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true},
     {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}, {'x':false, 'o':false, 'empty': true}
 ];
+
 var win_condition = function(){
     self = this;
     self.check_row_win = function(board, turn){
@@ -46,9 +47,8 @@ var win_condition = function(){
         var num_rows = Math.sqrt(board.length);
         for(var i=0;i<row_length - 1;i++){
             var rows_filled = 0;
-            var index = (i * row_length) + j;
             for(var j=0;j<num_rows - 1; j++){
-                if(board[index][turn]){
+                if(board[(i * row_length) + j][turn]){
                     rows_filled++;
                 }
                 else{
@@ -63,11 +63,10 @@ var win_condition = function(){
     self.check_col_win = function(board,turn){
         var col_length = Math.sqrt(board.length);
         var num_cols = Math.sqrt(board.length);
-        var index = (i * col_length) + j;
-        for(var i=0;i<col_length - 1;i++){
+        for(var i=0;i<col_length - 1;i+=col_length){
             var cols_filled = 0;
-            for(var j=0;j<num_cols - 1; j++){
-                if(board[index][turn]){
+            for(var j=0;j<num_cols - 1; j+=num_cols){
+                if(board[(i * col_length) + j][turn]){
                     cols_filled++;
                 }
                 else{
@@ -77,6 +76,32 @@ var win_condition = function(){
             if(cols_filled == col_length){
                 return true;
             }
+        }
+    }
+
+    self.check_diagonal_win = function(board,turn){
+        var col_length = Math.sqrt(board.length);
+        var num_cols = Math.sqrt(board.length);
+        var right_diagonals_filled = 0;
+        var left_diagonals_filled = 0;
+        for(var i=0;i<board.length - 1;i+=(col_length-1)) {
+            if (board[i][turn]) {
+                right_diagonals_filled++;
+            }
+            else{
+                break;
+            }
+        }
+        for(var j=0;j<num_cols - 1; j+=(col_length+1)){
+            if(board[i][turn]){
+                left_diagonals_filled++;
+            }
+            else{
+                break;
+            }
+        }
+        if(right_diagonals_filled || left_diagonals_filled == col_length){
+            return true;
         }
     }
 }
